1ea76292fabc3da8e246155030629c63485831bf
Faanns01 <ardiansyahfarhan248@gmail.com>
2025-07-09 19:44:02 +0700

    Pembaruan Data.js

--- backend/data.js
+++ backend/data.js
@@ -1,19 +1,34 @@
 const educationHistory = [
-  { id: 1, period: '2023 - Sekarang', institution: 'Universitas Amikom Yogyakarta', major: 'S1 - Informatika' }
+  { id: 1, period: '2023 - Sekarang', institution: 'Universitas Amikom Yogyakarta', major: 'S1 - Informatika' },
+   { id: 2, period: '2020 - 2023', institution: 'SMK 1 Muhammadiyah Muntilan', major: 'RPL-REKAYASA PERANGKAT LUNAK' },
+    { id: 3, period: '2017 - 2019', institution: 'SMP N 40 Batam' },
+    { id: 4, period: '2011 - 2016', institution: 'SD It At-Thoriq Batam' }
 ];
 
 const skills = [
+  { name: 'JavaScript', level: 'Mahir' },
+  { name: 'HTML', level: 'Mahir' },
+  { name: 'CSS', level: 'Mahir' },
+  { name: 'Tailwind', level: 'Mahir' },
+  { name: 'Vue Js', level: 'Mahir' },
+  { name: 'Bootstrap', level: 'Mahir' },
+  { name: 'Laravel', level: 'Mahir' },
   { name: 'JavaScript', level: 'Mahir' }
+
+
 ];
 
 const projects = [
   {
-    title: 'Website Toko Online',
+    title: 'Website Technologia',
     image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKtTULrrm5CG0jxRk1KYDUgs7lHxunHYIzcw&s',
-    description: 'Platform e-commerce dengan fitur keranjang belanja.',
+    description: 'Spesifikasi Laptop',
     tech: ['Vue.js', 'Express.js', 'PostgreSQL'],
-    link: '#'
+    link: 'https://github.com/ddaannuu/Project-Website-Laptop'
+  
+
   }
+
 ];
 
 module.exports = { educationHistory, skills, projects };
